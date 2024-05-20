1d34537e2002b494de1b82d73fae81d0e91a6ea7
Anthony35540 <98696420+Anthony35540@users.noreply.github.com>
2024-05-20 16:20:00 +0200

    Update slide.js
    mise a jour js avec pause du slider au survole de la souris

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,8 +1,40 @@
+
+// $(document).ready(function () {
+//     $('.customer-logos').slick({
+//         slidesToShow: 2,
+//         slidesToScroll: 1,
+//         autoplay: false,
+//         autoplaySpeed: 3500,
+//         arrows: false,
+//         dots: false,
+//         pauseOnHover: false,
+//         responsive: [{
+//             breakpoint: 979,
+//             settings: {
+//                 slidesToShow: 2
+//             }
+//         },{
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 1
+//             }
+//         },{
+//             breakpoint: 520,
+//             settings: {
+//                 slidesToShow: 1
+//             }
+//         }]
+//     });
+// });
+
+
+
 $(document).ready(function () {
-    $('.customer-logos').slick({
+    // Initialize the slider
+    var slider = $('.customer-logos').slick({
         slidesToShow: 2,
         slidesToScroll: 1,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 3500,
         arrows: false,
         dots: false,
@@ -24,5 +56,24 @@ $(document).ready(function () {
             }
         }]
     });
-});
 
+    // Function to stop the slider
+    function stop_slider() {
+        slider.slick('slickPause');
+    }
+
+    // Pause the slider on mouse enter
+    $('.customer-logos').on('mouseenter', function() {
+        stop_slider();
+    });
+
+    // Play the slider on mouse leave
+    $('.customer-logos').on('mouseleave', function() {
+        slider.slick('slickPlay');
+    });
+
+    // Add click event listener to each iframe
+    $('.iframe').on('click', function() {
+        stop_slider();
+    });
+});
